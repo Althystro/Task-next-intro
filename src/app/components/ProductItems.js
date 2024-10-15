cffc3090946f8374a344bda49482d2512b6c03cc
Abdulwahab-Alawadhi <abdulwahab.alawadhi56@gmail.com>
2024-10-15 10:50:16 +0300

    task 1 done

---
+++ src/app/components/ProductItems.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+function ProductItems() {
+  return (
+    <div>
+      <h1>Cookies and Beyond</h1>
+      <p>When cookie maniacs gather </p>
+      <img
+        src={
+          "https://www.cookietakeabite.com/wp-content/uploads/2020/02/cookietakeabitecookiecounter.jpg"
+        }
+        alt="Logo"
+      />
+    </div>
+  );
+}
+
+export default ProductItems;
