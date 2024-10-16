84e87bca9aac6083eab4c1090c59dd26f5040e0a
Abdulwahab-Alawadhi <abdulwahab.alawadhi56@gmail.com>
2024-10-16 08:19:04 +0300

    done

--- src/app/components/ProductItems.js
+++ src/app/components/ProductItems.js
@@ -1,12 +1,34 @@
 import React from "react";
+import Image from "next/image";
+import Products from "../products";
 
-function ProductItems() {
+// Task 5
+// function ProductItems() {
+//   const cookieslist = Products.map((product) => (
+//     <div class="p-10 flex flex-col justify-center items-center">
+//       <Image src={product.image} alt="Logo" width={100} height={100} />
+//       <h1 class=" font-bold ">{product.name}</h1>
+//       <h1 class=" font-bold ">{product.price}</h1>
+//     </div>
+//   ));
+//   return <>{cookieslist}</>;
+// }
+function ProductItems({ name, price, image }) {
   return (
-    <div class="flex flex-col justify-center items-center	">
-      <h1 class=" font-bold text-7xl">Cookies and Beyond</h1>
-      <h4 class=" font-bold text-3xl">When cookie maniacs gather </h4>
+    <div class="p-10 flex flex-col justify-center items-center">
+      <Image src={image} alt="Logo" width={100} height={100} />
+      <h1 class=" font-bold ">{name}</h1>
+      <h1 class=" font-bold ">{price}</h1>
     </div>
   );
 }
+
+// const Cookieslist = Products.map((product) => (
+//   <div class="p-10 flex flex-col justify-center items-center">
+//     <Image src={product.image} alt="Logo" width={100} height={100} />
+//     <h1 class=" font-bold ">{product.name}</h1>
+//     <h1 class=" font-bold ">{product.price}</h1>
+//   </div>
+// ));
 
 export default ProductItems;
--- src/app/page.js
+++ src/app/page.js
@@ -1,30 +1,49 @@
+"use client";
+
 import Image from "next/image";
 import cookieShop from "./assets/cookie_shop.jpg";
 import ProductItems from "./components/ProductItems";
 import Products from "./products";
+import { useState } from "react";
 
-const productsName = Products.map((product) => product.name);
-const productsPrice = Products.map((product) => product.price);
-const productsImage = Products.map((product) => product.image);
-
-const cookieslist = Products.map((product) => (
-  <div class="p-10 flex flex-col justify-center items-center">
-    <Image src={product.image} alt="Logo" width={100} height={100} />
-    <h1 class=" font-bold ">{product.name}</h1>
-    <h1 class=" font-bold ">{product.price}</h1>
-  </div>
+const notUsed = Products.map((product) => (
+  <ProductItems
+    price={product.price}
+    name={product.name}
+    image={product.image}
+  />
 ));
 
 export default function Home() {
+  const [count, setCount] = useState(0);
+
+  const handleButtonClick = () => {
+    setCount(count + 1);
+  };
   return (
     <div>
-      <ProductItems />
-
+      <div class="flex flex-col justify-center items-center	">
+        <h1 class=" font-bold text-7xl">Cookies and Beyond</h1>
+        <h4 class=" font-bold text-3xl">When cookie maniacs gather </h4>
+      </div>
+      <div class="flex flex-col justify-center items-center	">
+        <p> the count is {count}</p>
+        <button class="" onClick={handleButtonClick}>
+          Add +1
+        </button>
+      </div>
       <div class="flex flex-row justify-center items-center	">
         <Image src={cookieShop} alt="Logo" width={500} height={400} />
       </div>
-      <div class="flex flex-row justify-center items-center w-100 h-100 	">
-        {cookieslist}
+      <div class="flex flex-row justify-center items-center 	">
+        {/* 
+        Task 4
+        */}
+        {notUsed}
+
+        {/* 
+        Task 2
+        */}
 
         {/* <div class="p-10 flex flex-col justify-center items-center">
           <Image src={productsImage[0]} alt="Logo" width={100} height={100} />
